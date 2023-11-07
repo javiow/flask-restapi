2aaf224738b894613378756da5224591c44935e3
javiow <javiow@naver.com>
2023-11-07 21:50:29 +0900

    feat(sql): define sql_controller
    SQL 명령어를 실행해주는 함수

--- app/db.py
+++ app/db.py
@@ -1,1 +1,32 @@
 import sqlite3
+
+def sql_controller(query, var=None, db='test.db'):
+    """SQL 명령어를 실행해주는 함수
+
+    Args:
+        query: SQL 쿼리 (e.g. SELECT * FROM TABLE)
+        var: query안에 들어가는 변수, 2차원 이상의 튜플 형식
+            (e.g. (var1, ))
+        db: 현재 경로에 있는 db 파일
+
+    Returns:
+        쿼리가 SELECT문일 경우 패치한 결과를 반환, 그렇지 않은 경우는 None 반환
+
+    """
+
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    if var is None:
+        cur.execute(query)
+    else:
+        cur.execute(query, var)
+
+    if query.strip()[:6] == 'SELECT':
+        data = cur.fetchall()
+        return data
+    
+    conn.commit()
+    conn.close()
+
+    return None
